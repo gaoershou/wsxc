58b083df4cab7caca915780c43b2a3745895be15
wameng888 <cuizhaoyang@gaoershou.com>
2019-11-05 16:42:44 +0800

    '线上签约的配置文件'

---
+++ config/onlineSign.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: EDZ
+ * Date: 2019/11/5
+ * Time: 15:06
+ */
+return [
+    'prefixUrl' => 'https://smlopenapi.esign.cn',
+    'projectId' => '4438770529',
+    'projectsecret' => 'f951c7218e4f5d2972806ed0e3cc2d9a',
+    'prefix' => 'online_sign::',
+];
