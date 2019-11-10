f6fa59285dff5dd1c569c00facea7e862230b6ee
hanliyang <15811034090@163.com>
2019-11-11 00:00:15 +0800

    更换e签报账号和秘钥

--- config/onlineSign.php
+++ config/onlineSign.php
@@ -6,8 +6,8 @@
  * Time: 15:06
  */
 return [
-    'prefixUrl' => 'https://openapi.esign.cn',
-    'projectId' => '5111585557',
-    'projectsecret' => 'a8992f76f774277be7c21da02ea82db9',
+    'prefixUrl' => 'https://smlopenapi.esign.cn',
+    'projectId' => '4438770529',
+    'projectsecret' => 'f951c7218e4f5d2972806ed0e3cc2d9a',
     'prefix' => 'online_sign::',
 ];
